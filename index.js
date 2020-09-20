4ec41944907710d6a2cdd32640bd669ac742001b
AhmadHirthani <hertani86@gmail.com>
2020-09-20 16:20:50 +0300

    firistInit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const server = require('./lib/server');
+const mongoose = require('mongoose');
+require('dotenv');
+const db_url = process.env.db_url;
+let port = process.env.port;
+
+const mongooseOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(db_url, mongooseOptions);
+
+server.start(port);
